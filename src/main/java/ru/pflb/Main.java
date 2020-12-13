972b2ef02465eab310287833629764d068be6a27
ye <evgeny.golovnya@x5.ru>
2020-12-13 14:23:08 +0300

    gradle config

--- src/main/java/ru/pflb/Main.java
+++ src/main/java/ru/pflb/Main.java
@@ -3,6 +3,6 @@ package ru.pflb;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from maven project!");
+        System.out.println("Hello from Gradle project!");
     }
 }
